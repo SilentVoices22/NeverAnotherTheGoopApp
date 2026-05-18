a25ea0a853f223e38919098d71b5a71c59badc7c
SilentVoices22 <pinyuanlu@gmail.com>
2026-05-18 23:09:55 +0200

    adding landing page

--- app/src/main/java/com/example/neveranotherappthegoop/MainActivity.kt
+++ app/src/main/java/com/example/neveranotherappthegoop/MainActivity.kt
@@ -30,18 +30,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello skibidi",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NeverAnotherAppTheGoopTheme {
-        Greeting("Android")
-    }
-}
