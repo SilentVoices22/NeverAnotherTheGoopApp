9e76e43b989e3935a955d3a9dbdd7f52753f7fd1
SilentVoices22 <pinyuanlu@gmail.com>
2026-05-20 23:10:37 +0200

    new Icons, updated more on landing page

--- app/src/main/java/com/example/neveranotherappthegoop/landingPage.kt
+++ app/src/main/java/com/example/neveranotherappthegoop/landingPage.kt
@@ -1,6 +1,7 @@
 package com.example.neveranotherappthegoop
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.focusGroup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,28 +11,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.neveranotherappthegoop.data.IcMenu
 import com.example.neveranotherappthegoop.data.IcShoppingCart
+import com.example.neveranotherappthegoop.data.IcTapeMeasure
 import com.example.neveranotherappthegoop.data.NALogoName
+import com.example.neveranotherappthegoop.data.NaAbout1
+import com.example.neveranotherappthegoop.data.PoseAnklePic
+import com.example.neveranotherappthegoop.data.ThreeStepList
+import com.example.neveranotherappthegoop.ui.theme.CharcoalGrey
 import org.w3c.dom.Text
 
 @Composable
-fun Topbar(){
-    Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp),
+fun Topbar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp, vertical = 20.dp),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween){
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         IcMenu()
         NALogoName()
         IcShoppingCart()
@@ -40,32 +55,81 @@ fun Topbar(){
 
 @Preview(showBackground = true)
 @Composable
-fun LandingPageBodyPreview(){
-   LandingPageBody()
+fun LandingPageBodyPreview() {
+    LandingPageBody()
+
 }
 
 
 @Composable
-fun LandingPageText(){
- Column(modifier = Modifier.fillMaxWidth().padding( start = 24.dp, top = 24.dp, end = 24.dp)) {
-     Box(modifier = Modifier.wrapContentSize()) {
-         Text(text = "A Bra that's made for you.",
-             fontSize = 42.sp,
-             fontWeight = FontWeight.Bold,
-             fontFamily = FontFamily.Monospace)
-         Spacer(modifier = Modifier.height(12.dp))
-         Text(text = "Finally. Comfort that fits your body and your life")
-     }
+fun LandingPageText() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 24.dp, top = 24.dp, end = 24.dp)
+    ) {
+        Column(modifier = Modifier.padding(end = 40.dp)) {
+            Text(
+                text = "A Bra that's tailored for you.",
+                fontSize = 37.sp,
+                fontWeight = FontWeight.Medium,
+                style = MaterialTheme.typography.titleLarge.copy(),
+                lineHeight = 40.sp,
+                color = CharcoalGrey
+            )
 
- }
-}
-<!--- Kevin --->
-@Composable
-fun LandingPageBody(){
-    Column(modifier = Modifier.fillMaxSize()) {
-        Topbar()
-        Spacer(modifier = Modifier.height(10.dp))
-        LandingPageText()
+            Spacer(modifier = Modifier.height(12.dp))
 
+            Text(
+                text = "Finally. Comfort that fits your body and your life",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                style = MaterialTheme.typography.bodyMedium,
+                color = CharcoalGrey,
+            )
+        }
     }
 }
+
+@Composable
+fun LandingPagePicture() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        NaAbout1(
+            modifier = Modifier
+                .weight(1f)
+                .height(250.dp)
+        )
+        PoseAnklePic(
+            modifier = Modifier
+                .weight(1f)
+                .height(250.dp)
+        )
+    }
+}
+
+@Composable
+fun LandingPageBody() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 24.dp)
+    ) {
+        Topbar()
+        LandingPageText()
+        LandingPagePicture()
+        Column(verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()) {
+            ThreeStepList(modifier = Modifier
+                .height(80.dp)
+                .padding(top = 5.dp))
+        }
+        Text(
+            text = "The NEVER ANOTHER wire free bra is tailored to perfectly fit you. Using your measurements, our algorithm automatically customizes the bra to fit your body. ",
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Normal,
+            style = MaterialTheme.typography.bodyMedium,
+            color = CharcoalGrey,
+            modifier = Modifier.padding(start = 24.dp, end = 24.dp),
+            textAlign = TextAlign.Center)
+    }
+}
+
